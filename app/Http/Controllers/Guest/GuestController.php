1f54ebe8225d514b02ffdb929121334db7d19d7b
Musavi Ardabilly Taufik <201musavi@gmail.com>
2023-03-12 23:01:58 +0700

    Update GuestController

--- app/Http/Controllers/Guest/GuestController.php
+++ app/Http/Controllers/Guest/GuestController.php
@@ -19,4 +19,10 @@ class GuestController extends Controller
     public function logo(){
         return view('logo');
     }
+    public function login(){
+        return view('guest.login');
+    }
+    public function register(){
+        return view('guest.register');
+    }
 }
