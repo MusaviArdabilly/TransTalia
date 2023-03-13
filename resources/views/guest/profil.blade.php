d0d64574552b252ba1e88f71ccddc976a5ac6afc
Musavi Ardabilly Taufik <201musavi@gmail.com>
2023-03-14 05:17:33 +0700

    Update profil - map iframe

--- resources/views/guest/profil.blade.php
+++ resources/views/guest/profil.blade.php
@@ -47,7 +47,7 @@
             </div>
         </div>
         <div class="container">
-            <iframe class="rounded" src="https://maps.google.com/maps?q=transtalia&t=&z=10&ie=UTF8&iwloc=&output=embed" width="100%" height="380" frameborder="0" style="border:0" allowfullscreen></iframe>
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d506247.028906027!2d112.26581816869665!3d-7.568992532492812!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e786c590eec46d3%3A0xd3b75dc4dc30a5d7!2sTRANSTALIA%20BUS%20GARAGE!5e0!3m2!1sid!2sid!4v1678680509527!5m2!1sid!2sid" width="100%" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
         </div>
     </section>
 
