f94dbd32d0a7381a5ea19d2f135b8a6311d71547
Musavi Ardabilly Taufik <201musavi@gmail.com>
2023-05-21 03:55:25 +0700

    Database - Seeder adjustment

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,7 +2,6 @@
 
 namespace Database\Seeders;
 
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -29,5 +28,6 @@ class DatabaseSeeder extends Seeder
         $this->call(UserSeeder::class);
         $this->call(ArmadaBusSeeder::class);
         $this->call(KodePerawatanSeeder::class);
+        $this->call(PegawaiSeeder::class);
     }
 }
