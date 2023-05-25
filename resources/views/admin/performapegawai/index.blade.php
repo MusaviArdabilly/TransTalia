7249ff118973e0d75a61d966e08aee8a4f854dcb
Musavi Ardabilly Taufik <201musavi@gmail.com>
2023-05-25 16:12:44 +0700

    View - PerformaPegawai adjustment

--- resources/views/admin/performapegawai/index.blade.php
+++ resources/views/admin/performapegawai/index.blade.php
@@ -32,6 +32,20 @@
         </div>
 
         <div class="col-12 minvh100-233">
+            
+            <div class="card shadow mb-4">
+                <!-- Card Header - Dropdown -->
+                <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
+                    <h6 class="m-0 font-weight-bold text-primary">Performa Pegawai</h6>
+                </div>
+                <!-- Card Body -->
+                <div class="card-body">
+                    <div class="chart-bar">
+                        <canvas id="myBarChart"></canvas>
+                    </div>
+                </div>
+            </div>
+
             <div class="card shadow mb-4">
                 <!-- Card Header - Dropdown -->
                 <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
@@ -72,19 +86,6 @@
                                     @endforelse
                             </tbody>
                         </table>
-                    </div>
-                </div>
-            </div>
-            
-            <div class="card shadow mb-4">
-                <!-- Card Header - Dropdown -->
-                <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
-                    <h6 class="m-0 font-weight-bold text-primary">Performa Pegawai</h6>
-                </div>
-                <!-- Card Body -->
-                <div class="card-body">
-                    <div class="chart-bar">
-                        <canvas id="myBarChart"></canvas>
                     </div>
                 </div>
             </div>
