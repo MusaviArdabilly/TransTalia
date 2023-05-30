e293f145ca832c638f5cb2868a5886c5cc0997cc
Musavi Ardabilly Taufik <201musavi@gmail.com>
2023-05-30 10:21:09 +0700

    Seeder - DatabaseSeeder adjustment
    remove unused code

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -11,18 +11,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
-        
-        // $this->call(ProvincesSeeder::class);
-        // $this->call(CitiesSeeder::class);
-        // $this->call(DistrictsSeeder::class);
-        // $this->call(VillagesSeeder::class);
-
         //use php artisan laravolt:indonesia:seed
 
         $this->call(UserSeeder::class);
