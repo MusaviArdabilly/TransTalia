4eeba33aa0bdcd1706e807f18e0091f55412c23c
Musavi Ardabilly Taufik <201musavi@gmail.com>
2023-05-30 10:31:59 +0700

    View - Guest calendar adjustment
    remove unused code and setup fullcalendar

--- resources/views/guest/jadwal.blade.php
+++ resources/views/guest/jadwal.blade.php
@@ -9,18 +9,13 @@
 </script>
 
 <div class="fake-navbar"></div>
-<div class="minvh100-114">
-    <div class="container my-5" id="calendar-container">
-        <div id="calendar"></div>
-    </div>
-
-    <div class="container my-5" id="calendar-container">
-        <iframe src="https://calendar.google.com/calendar/embed?height=600&wkst=2&bgcolor=%23ffffff&ctz=Asia%2FJakarta&showTitle=0&showPrint=0&showTabs=1&showCalendars=0&hl=id&src=YWxleDBtY2NsYW5lQGdtYWlsLmNvbQ&src=YWRkcmVzc2Jvb2sjY29udGFjdHNAZ3JvdXAudi5jYWxlbmRhci5nb29nbGUuY29t&src=MjU2ODYyYzEzOTcxOWZjZWMzYzViY2I0OWE3YTEzYzBhM2IwYmE2ZGQ4ODI2NjI3ZmI2YTg5MWZjZGRhYmVhNEBncm91cC5jYWxlbmRhci5nb29nbGUuY29t&src=aWQuaW5kb25lc2lhbiNob2xpZGF5QGdyb3VwLnYuY2FsZW5kYXIuZ29vZ2xlLmNvbQ&color=%23039BE5&color=%2333B679&color=%23EF6C00&color=%230B8043" style="border-width:0" width="100%" height="100%" frameborder="0" scrolling="no"></iframe>
-    </div>
-
-    <div class="container my-5">
+<div class="minvh100-114 d-flex align-items-center px-0 py-3 p-md-3">
+    <div class="container-md">
+        <div class="col-12 mb-3">
+            <div id="calendar"></div>
+        </div>
         <div class="col-12 col-md-6 mx-auto">
-            <div class="alert alert-primary text-center" role="alert">
+            <div class="alert alert-primary text-center p-1 my-0" role="alert">
                 Sudah menentukan jadwal? <br> 
                 @if(Auth::check())
                 Reservasi sekarang! <br>
@@ -36,18 +31,33 @@
     
     {{-- FullCalendar --}}
     <script src='{{ asset('vendor/fullcalendar/dist/index.global.js') }}'></script>
-    <script src='{{ asset('vendor/fullcalendar/package/core/locales/id.global.min.js') }}'></script>
+    <script src='{{ asset('vendor/fullcalendar/packages/core/locales/id.global.min.js') }}'></script>
+    <script src='{{ asset('vendor/fullcalendar/packages/bootstrap5/index.global.min.js') }}'></script>
     <script>
         document.addEventListener('DOMContentLoaded', function() {
-          var calendarEl = document.getElementById('calendar');
-          var calendar = new FullCalendar.Calendar(calendarEl, {
+            var jadwal = @json($events);
+            var calendarEl = document.getElementById('calendar');
+            var calendar = new FullCalendar.Calendar(calendarEl, {
+                timeZone: 'Asia/Jakarta',
                 locale: 'id',
                 themeSystem: 'bootstrap5',
-                contentHeight: '100%',
-                contentWidth: '100%',
-                initialView: 'dayGridMonth'
-          });
-          calendar.render();
+                initialView: 'multiMonthYear',
+                multiMonthMaxColumns: 2,
+                headerToolbar: false,
+                events: jadwal,
+                eventClick: function(info) {
+                    info.jsEvent.preventDefault(); // Prevent the default click action
+                    $(info.el).popover({
+                        content: info.event.title,
+                        trigger: 'hover',
+                        placement: 'top',
+                        container: 'body',
+                        html: true
+                    });
+                    $(info.el).popover('toggle'); // Show the popover
+                }
+            });
+            calendar.render();
         });
     </script>
 
