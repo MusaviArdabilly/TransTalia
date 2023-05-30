bb8b130c8221b095cf462dd4c0354b9d7901e31e
Musavi Ardabilly Taufik <201musavi@gmail.com>
2023-05-30 10:13:21 +0700

    Controller - Guest adjustment
    add array for fullcalendar

--- app/Http/Controllers/Guest/GuestController.php
+++ app/Http/Controllers/Guest/GuestController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers\Guest;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\ReservasiArmadaBus;
+use Carbon\Carbon;
 
 class GuestController extends Controller
 {
@@ -14,8 +16,34 @@ class GuestController extends Controller
         return view('guest.profil');
     }
     public function jadwal(){
-        return view('guest.jadwal');
+        $events = array();
+        $schedules = ReservasiArmadaBus::with('reservasi', 'armada_bus')->get();
+
+        foreach($schedules as $schedule){
+            $parts = explode(',', $schedule->reservasi->kota_tujuan);
+            $kota_tujuan = array_pop($parts);
+            $kota_tujuan = implode(', ', $parts);
+            $events[] = [
+                'title' => $schedule->armada_bus->nama . ' - ' . $kota_tujuan,
+                'start' => Carbon::parse($schedule->reservasi->tanggal_mulai)->setTime(8, 00, 00)->setTimezone('Asia/Jakarta')->toDateTimeString(),
+                'end' => Carbon::parse($schedule->reservasi->tanggal_selesai)->addDay()->setTime(23, 59, 59)->setTimezone('Asia/Jakarta')->toDateTimeString(),
+                'backgroundColor' => self::getStatusColor($schedule->reservasi->status),
+                'borderColor' => '#000',
+                'allDay' => true
+            ];
+        }
+
+        return view('guest.jadwal', compact('events'));
     }
+        
+    public static function getStatusColor($status){
+        if ($status == 'menunggu'){
+            return '#FFC107';
+        }else {
+            return '';
+        }
+    }
+
     public function logo(){
         return view('logo');
     }
