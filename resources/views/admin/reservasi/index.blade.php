2d107fb160bd8f5ff10e341d41ac392c3b0e0b24
Musavi Ardabilly Taufik <201musavi@gmail.com>
2023-05-30 10:31:23 +0700

    View - Reservasi adjustment

--- resources/views/admin/reservasi/index.blade.php
+++ resources/views/admin/reservasi/index.blade.php
@@ -1,5 +1,5 @@
 @extends('admin.layout')
-@section('title', 'Pegawai')
+@section('title', 'Reservasi')
 @section('content')
 
 <script type="text/javascript">
@@ -11,12 +11,12 @@
 
         <!-- Page Heading -->
         <div class="d-sm-flex align-items-center justify-content-between mb-4">
-            <div class="d-none d-sm-inline-block">
+            <div class="d-none d-sm-inline-block mr-3">
                 <!-- Topbar Search -->
                 <form method="GET" action="/admin/reservasi"
                     class="d-none d-sm-inline-block form-inline mr-auto ml-md-3 my-2 my-md-0 mw-100 navbar-search dropshadowlight">
                     <div class="input-group">
-                        <input type="text" name="search" value="" class="form-control bg-light border-0 small" placeholder="Pencarian"
+                        <input type="text" name="search" value="{{ $search_key }}" class="form-control bg-light border-0 small" placeholder="Pencarian"
                             aria-label="Search" aria-describedby="basic-addon2">
                         <div class="input-group-append">
                             <button class="btn btn-primary" type="submit">
@@ -26,57 +26,88 @@
                     </div>
                 </form>
             </div>
-            <div class="d-none d-sm-inline-block btn bg-primary text-white shadow-sm">
-                {{ str_replace('-', ' ', now()->format('d-M-y')) }}
+            <div class="d-none d-sm-inline-block btn bg-primary disabled text-white shadow-sm">
+                {{ \Carbon\Carbon::parse(now())->locale('id')->isoFormat('DD MMM YY') }}
             </div>
         </div>
 
         <div class="col-12 minvh100-233">
             <div class="card shadow mb-4">
                 <!-- Card Header - Dropdown -->
-                <div class="card-header py-3 d-flex flex-row align-items-center justify-content-start">
+                <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
                     <h6 class="m-0 font-weight-bold text-primary">Reservasi</h6>
+                    <div>
+                        <form method="GET" action="/admin/reservasi"
+                            class="d-none d-sm-inline-block form-inline mr-auto ml-md-3 my-2 my-md-0 mw-100 navbar-search dropshadowlight">
+                            <div class="input-group">
+                                <select class="form-control bg-light border-0 small" name="filter">
+                                    <option {{ $filter === '' ? 'selected' : '' }} value="">Semua</option>
+                                    <option {{ $filter === 'menunggu' ? 'selected' : '' }} value="menunggu">Menunggu</option>
+                                    <option {{ $filter === 'dibayar' ? 'selected' : '' }} value="dibayar">Dibayar</option>
+                                    <option {{ $filter === 'lunas' ? 'selected' : '' }} value="lunas">Lunas</option>
+                                    <option {{ $filter === 'batal' ? 'selected' : '' }} value="batal">Batal</option>
+                                </select>
+                                <div class="input-group-append">
+                                    <button class="btn btn-primary" type="submit">
+                                        <i class="fas fa-filter"></i>
+                                    </button>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
                 </div>
                 <!-- Card Body -->
                 <div class="card-body table-responsive">
-                    <table class="table">
+                    <table class="table table-hover">
                         <thead>
                           <tr>
                             <th scope="col">#</th>
                             {{-- <th scope="col">@sortablelink('user.nama_depan', ' Nama', [], ['class' => 'text-decoration-none text-secondary'])</th> --}}
+                            <th scope="col">Kode</th>
                             <th scope="col">Nama Penyewa</th>
-                            <th scope="col">Tanggal Mulai</th>
-                            <th scope="col">Tanggal Selesai</th>
+                            <th scope="col">Waktu</th>
                             <th scope="col">Tujuan</th>
+                            <th scope="col">Armada Bus</th>
                             <th scope="col">Total Harga</th>
                             <th scope="col">Dibayar</th>
-                            <th scope="col">Status</th>
+                            <th scope="col" class="text-center">Status</th>
                             <th scope="col" class="text-center">Aksi</th>
                           </tr>
                         </thead>
                         <tbody>
-                            @forelse ($data_reservasi as $key => $reservasi)
+                            @forelse($data_reservasi as $key => $reservasi)
                             <tr>
                                 <th class="text-capitalize" scope="row">{{ $data_reservasi->firstItem()+$key }}</th>
-                                <td class="text-capitalize">{{ $reservasi->user->nama_depan }} {{ $reservasi->user->nama_belakang }}</td>
-                                <td class="text-capitalize">{{ $reservasi->tanggal_mulai }}</td>
-                                <td class="text-capitalize">{{ $reservasi->tanggal_selesai }}</td>
-                                <td class="text-capitalize">{{ $reservasi->tujuan }}</td>
-                                <td class="text-capitalize">{{ $reservasi->total_harga }}</td>
-                                <td class="text-capitalize">{{ $reservasi->dibayar }}</td>
-                                <td class="text-capitalize">{{ $reservasi->status }}</td>
-                                <td>
+                                <td class="text-capitalize">{{ $reservasi->kode }}</td>
+                                <td class="text-capitalize">{{ $reservasi->user->nama_depan }} <br> {{ $reservasi->user->nama_belakang }}</td>
+                                <td class="text-capitalize">{{ \Carbon\Carbon::parse($reservasi->tanggal_mulai)->locale('id')->isoFormat('DD MMM YYYY') }} <br> {{ \Carbon\Carbon::parse($reservasi->tanggal_selesai)->locale('id')->isoFormat('DD MMM YYYY') }}</td>
+                                @php
+                                    $value = $reservasi->kota_tujuan;
+                                    // Split the string by commas
+                                    $parts = explode(',', $value);
+                                @endphp
+                                <td class="text-capitalize">{{ $parts[0] }} <br> {{ $parts[1] }} <br> {{ $parts[2] }}</td>
+                                <td class="text-capitalize">
+                                    @foreach($reservasi->reservasi_armada_bus as $data_armada_bus)
+                                    {{ $data_armada_bus->armada_bus->nama }} - Rp. {{ number_format($data_armada_bus->sub_total, 0, ',', '.') }} <br>
+                                    @endforeach
+                                </td>
+                                <td class="text-capitalize">Rp. {{ number_format($reservasi->total_harga, 0, ',', '.') }}</td>
+                                <td class="text-capitalize">Rp. {{ number_format($reservasi->dibayar, 0, ',', '.') }}</td>
+                                <td class="text-capitalize text-center">
+                                    <span class="badge rounded-pil text-capitalize text-light w-100 {{ $reservasi->status === 'batal' ? 'bg-danger' : '' }} {{ $reservasi->status === 'menunggu' ? 'bg-secondary' : '' }} {{ $reservasi->status === 'dibayar' ? 'bg-primary' : '' }} {{ $reservasi->status === 'lunas' ? 'bg-success' : '' }}">
+                                        {{  ucwords(str_replace('_', ' ', $reservasi->status)) }}
+                                    </span>
+                                </td>
+                                <td class="text-center">
                                     <a href="{{ url('admin/reservasi/ubah/'.$reservasi->id) }}" class="text-decoration-none">
                                         <i class="fas fa-edit text-warning"></i>&nbsp; 
-                                    </a>
-                                    <a href="{{ url('admin/reservasi/hapus/'.$reservasi->id) }}" class="text-decoration-none" onclick="return confirm('Apakah anda yakin untuk menghapus data Reservasi - {{ $reservasi->user->nama_depan }}?')">
-                                        <i class="fas fa-trash-alt text-danger"></i>
                                     </a>
                                 </td>
                             </tr>
                             @empty
                             <tr>
-                                <td colspan="9">
+                                <td colspan="10">
                                     <div class="alert alert-danger text-center" role="alert">
                                         Data Kosong
                                     </div>
@@ -85,6 +116,9 @@
                             @endforelse
                         </tbody>
                     </table>
+                    <div class="d-flex justify-content-end">
+                        {{ $data_reservasi->links() }}
+                    </div>
                 </div>
             </div>
         </div>
---
+++ resources/views/admin/reservasi/ubah.blade.php
@@ -0,0 +1,137 @@
+@extends('admin.layout')
+@section('title', 'Ubah Reservasi')
+@section('content')
+
+<script type="text/javascript">
+    document.getElementById('reservasi').classList.add('active');
+</script>
+
+    <!-- Begin Page Content -->
+    <div class="container-fluid py-2">
+
+        <div class="col-12 minvh100-233">
+            <div class="d-flex justify-content-center">
+                <div class="col-12 col-md-12">
+                    <div class="card shadow mb-4">
+                        <!-- Card Header - Dropdown -->
+                        <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
+                            <h6 class="m-0 font-weight-bold text-primary">Ubah Data Reservasi</h6>
+                        </div>
+                        <!-- Card Body -->
+                        <div class="card-body">
+                            <form method="POST" action="{{ url('/admin/reservasi/ubah/'.$reservasi->id.'/post') }}">
+                                @csrf
+                                <input type="hidden" name="tanggal_mulai_lama" value="{{ $reservasi->tanggal_mulai }}">
+                                <input type="hidden" name="tanggal_selesai_lama" value="{{ $reservasi->tanggal_selesai }}">
+                                <div class="row mb-3">
+                                    <div class="col-12 col-md-6">
+                                        <label for="inputKodeReservasi">Kode Reservasi</label>
+                                        <input type="text" readonly name="kode_reservasi" class="form-control" id="inputKodeReservasi" value="{{ old('kode', $reservasi->kode) }}">
+                                        @if ($errors->has('kode'))
+                                        <span class="ps-3 text-danger"><i class="fas fa-exclamation-circle"></i>&nbsp;{{ $errors->first('kode') }}</span>
+                                        @endif
+                                    </div>
+                                    <div class="col-12 col-md-6">
+                                        <label for="inputJabatan">Penyewa</label>
+                                        <select readonly name="user_id" class="form-control select2" aria-label="Default select" id="inputJabatan">
+                                            <option selected value="{{ $reservasi->user_id }}">{{ $reservasi->user->nama_depan }} {{ $reservasi->user->nama_belakang }}</option>
+                                        </select>
+                                        @if ($errors->has('user_id'))
+                                        <span class="ps-3 text-danger"><i class="fas fa-exclamation-circle"></i>&nbsp;{{ $errors->first('user_id') }}</span>
+                                        @endif
+                                    </div>
+                                </div>
+                                <div class="row mb-3">
+                                    <div class="col-12 col-md-6">
+                                        <label for="inputTanggalMulai">Tanggal Mulai</label>
+                                        <input type="date" readonly name="tanggal_mulai" class="form-control" id="inputTanggalMulai" value="{{ old('tanggal_mulai', $reservasi->tanggal_mulai) }}">
+                                        @if ($errors->has('tanggal_mulai'))
+                                        <span class="ps-3 text-danger"><i class="fas fa-exclamation-circle"></i>&nbsp;{{ $errors->first('tanggal_mulai') }}</span>
+                                        @endif
+                                    </div>
+                                    <div class="col-12 col-md-6">
+                                        <label for="inputTanggalMulai">Tanggal Selesai</label>
+                                        <input type="date" readonly name="tanggal_selesai" class="form-control" id="inputTanggalMulai" value="{{ old('tanggal_selesai', $reservasi->tanggal_selesai) }}">
+                                        @if ($errors->has('tanggal_selesai'))
+                                        <span class="ps-3 text-danger"><i class="fas fa-exclamation-circle"></i>&nbsp;{{ $errors->first('tanggal_selesai') }}</span>
+                                        @endif
+                                    </div>
+                                </div>
+                                <div class="row mb-3">
+                                    <div class="col-12 col-md-6">
+                                        <label for="inputTake">Kota Penjemputan</label>
+                                        <input readonly type="text" name="kota_jemput" class="form-control" id="inputTake" value="{{ old('kota_jemput', $reservasi->kota_jemput) }}">
+                                        @if ($errors->has('kota_jemput'))
+                                        <span class="ps-3 text-danger"><i class="fas fa-exclamation-circle"></i>&nbsp;{{ $errors->first('kota_jemput') }}</span>
+                                        @endif
+                                    </div>
+                                    <div class="col-12 col-md-6">
+                                        <label for="inputDeliver">Kota Tujuan</label>
+                                        <input readonly type="text" name="kota_tujuan" class="form-control" id="inputDeliver" value="{{ old('kota_tujuan', $reservasi->kota_tujuan) }}">
+                                        @if ($errors->has('kota_tujuan'))
+                                        <span class="ps-3 text-danger"><i class="fas fa-exclamation-circle"></i>&nbsp;{{ $errors->first('kota_tujuan') }}</span>
+                                        @endif
+                                    </div>
+                                </div>
+                                <div class="mb-3">
+                                    <label for="ArmadaBus">Armada Bus</label> <br>
+                                    @foreach($reservasi->reservasi_armada_bus as $data_armada_bus)
+                                        <span class="btn btn-primary disabled">{{ $data_armada_bus->armada_bus->nama }}</span>
+                                    @endforeach
+                                    @if ($errors->has('jumlah_order'))
+                                    <span class="ps-3 text-danger"><i class="fas fa-exclamation-circle"></i>&nbsp;{{ $errors->first('jumlah_order') }}</span>
+                                    @endif
+                                </div>
+                                <div class="row mb-3">
+                                    <div class="col-12 col-md-2">
+                                        <label for="inputJabatan">Status</label> 
+                                        <select name="status" class="form-control select2" aria-label="Default select" id="inputJabatan">
+                                            <option value="batal"{{ $reservasi->status == 'batal' ? 'selected' : '' }}>Batal</option>
+                                            <option value="menunggu"{{ $reservasi->status == 'menunggu' ? 'selected' : '' }}>Menunggu</option>
+                                            <option value="dibayar"{{ $reservasi->status == 'dibayar' ? 'selected' : '' }}>Dibayar</option>
+                                            <option value="lunas"{{ $reservasi->status == 'lunas' ? 'selected' : '' }}>Lunas</option>
+                                        </select>
+                                        @if ($errors->has('status'))
+                                        <span class="ps-3 text-danger"><i class="fas fa-exclamation-circle"></i>&nbsp;{{ $errors->first('status') }}</span>
+                                        @endif
+                                    </div>
+                                    <div class="col-12 col-md-4">
+                                        <label for="inputJumlahOrder">Total Harga</label>
+                                        <input type="number" name="total_harga" class="form-control" id="inputJumlahOrder" value="{{ old('total_harga', $reservasi->total_harga) }}">
+                                        @if ($errors->has('total_harga'))
+                                        <span class="ps-3 text-danger"><i class="fas fa-exclamation-circle"></i>&nbsp;{{ $errors->first('total_harga') }}</span>
+                                        @endif
+                                    </div>
+                                </div>
+                                <div class="d-flex justify-content-end">
+                                    <a href="{{ URL::previous() }}" class="btn btn-outline-secondary mr-4">Batal</a>
+                                    <button type="submit" class="btn btn-primary">Simpan</button>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+
+    </div>
+    <!-- End of Main Content -->
+
+    {{-- Handle autocomplate kota penjemputan dan kota tujuan  --}}
+    <script>
+        function cities(){
+            var options = {
+                types: ['(cities)'],
+                componentRestrictions: { country: 'id' } 
+            };
+            
+            var pickInput = document.getElementById('inputTake');
+            var pickInputAutocomplete = new google.maps.places.Autocomplete(pickInput, options);
+            
+            var deliverInput = document.getElementById('inputDeliver');
+            var deliverInputAutocomplete = new google.maps.places.Autocomplete(deliverInput, options);
+        }
+    </script>
+    <script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?key=AIzaSyD3NfQbLS6VzWjfJqKAa-2UiHYyzAlfMRI&libraries=places&language=id&callback=cities"></script>
+@endsection
